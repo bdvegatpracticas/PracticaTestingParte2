f4d610d38c2c6012323d05ca4c9fb33260c87193
bryan vega <bdvegat@unal.edu.co>
2019-07-09 21:20:54 -0500

    first commit

---
+++ src/components/loadingindicator.js
@@ -0,0 +1,13 @@
+import React, {Component} from "react";
+import PropTypes from "prop-types";
+
+
+export default class loadingIndicator extends component {
+    render(){
+        return null;
+    }
+}
+
+loadingIndicator.propTypes = {
+    isLoading : Proptypes.bool.isRequired
+};
